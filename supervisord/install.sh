80f11f3c006d135a4f397c6ef3597b856d26f366
Francisc Simon <francisc.simon@evalgo.org>
2019-10-09 11:52:06 +0200

    Update supervisord/install.sh, supervisord/barcode.conf files

--- supervisord/install.sh
+++ supervisord/install.sh
@@ -4,15 +4,14 @@ sudo apt-get install supervisor
 sudo systemctl enable supervisor
 sudo systemctl start supervisor
 
-sudo pip install evdev
-sudo pip2 install evdev
-pip install evdev
-pip2 install evde
+sudo pip3 install evdev
+pip3 install evdev
 
-udo pip install websockets
-sudo pip2 install websockets
-pip install websockets
-pip2 install websockets
+sudo pip3 install pathlib
+pip3 install pathlib
+
+sudo pip3 install websockets
+pip3 install websockets
 
 sudo cp barcode.conf /etc/supervisor/conf.d/barcode.conf
 sudo supervisorctl update
