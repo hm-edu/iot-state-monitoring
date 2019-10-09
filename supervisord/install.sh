e9a74be96da79d6b164c8ca7657054ab2495a488
Francisc Simon <francisc.simon@evalgo.org>
2019-10-09 11:48:45 +0200

    Update supervisord/install.sh, webhook/send.py, supervisord/barcode.conf, webhook/barcode_websocket_server.py files

--- supervisord/install.sh
+++ supervisord/install.sh
@@ -7,7 +7,12 @@ sudo systemctl start supervisor
 sudo pip install evdev
 sudo pip2 install evdev
 pip install evdev
-pip2 install evdev
+pip2 install evde
+
+udo pip install websockets
+sudo pip2 install websockets
+pip install websockets
+pip2 install websockets
 
 sudo cp barcode.conf /etc/supervisor/conf.d/barcode.conf
 sudo supervisorctl update
