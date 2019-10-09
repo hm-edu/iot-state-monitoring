ff63424bad34ecf95a0ea9ed7424a1a27a10e801
Francisc Simon <francisc.simon@evalgo.org>
2019-10-09 11:25:03 +0200

    updated install supervisor script

--- supervisord/install.sh
+++ supervisord/install.sh
@@ -4,5 +4,10 @@ sudo apt-get install supervisor
 sudo systemctl enable supervisor
 sudo systemctl start supervisor
 
+sudo pip install evdev
+sudo pip2 install evdev
+pip install evdev
+pip2 install evdev
+
 sudo cp barcode.conf /etc/supervisor/conf.d/barcode.conf
 sudo supervisorctl update
