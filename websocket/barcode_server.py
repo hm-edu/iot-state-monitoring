48d7ba2c281a22c63d36e01a7517713a7953ac82
Max <gaerber@axxepta.de>
2019-11-05 14:42:29 +0100

    change ips

--- websocket/barcode_server.py
+++ websocket/barcode_server.py
@@ -11,11 +11,13 @@ import websockets
 async def barcodeFn(websocket, path):
     await websocket.recv()
     while True:
-        barcodeTxt = read.barcode('/dev/input/event1')
+        barcodeTxt = read.barcode('/dev/input/event0')
         print(barcodeTxt)
         await websocket.send(barcodeTxt)
 
+#start_server = websockets.serve(barcodeFn, "127.0.0.1", 8080)
 start_server = websockets.serve(barcodeFn, "0.0.0.0", 8080)
+
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
