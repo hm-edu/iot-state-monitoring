a934ced547e28e3967bab01114d6cabbe361c5ad
Max Gaerber <gaerber@axxepta.de>
2019-11-06 19:08:58 +0100

    add sender to client py

--- websocket/client.py
+++ websocket/client.py
@@ -1,3 +1,4 @@
+import read
 import asyncio
 import websockets
 
@@ -6,5 +7,13 @@ async def getBarCode():
     async with websockets.connect(uri) as websocket:
         barcodeTxt = await websocket.recv()
         print(barcodeTxt)
+        
+async def sendBarCode():
+    uri = "ws://127.0.0.1:8080"
+    async with websockets.connect(uri) as websocket:
+         while True:
+            barcodeTxt = read.barcode('/dev/input/event0')
+            print(barcodeTxt)
+            await websocket.send(barcodeTxt))
 
-asyncio.get_event_loop().run_until_complete(getBarCode())
+asyncio.get_event_loop().run_until_complete(sendBarCode())
