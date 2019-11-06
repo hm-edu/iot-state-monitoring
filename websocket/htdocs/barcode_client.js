8f53bfa25ae305b1bc86e5bf1b90fe70a57126b4
Max Gaerber <gaerber@axxepta.de>
2019-11-06 16:46:20 +0100

    nicer messages

--- websocket/htdocs/barcode_client.js
+++ websocket/htdocs/barcode_client.js
@@ -2,9 +2,20 @@ $( document ).ready(function() {
 
     // 8020 nginx proxy
     //var url = 'ws://127.0.0.1:8020/';
-    var url = 'ws://192.168.177.81:8080/';
+    var url = 'ws://192.168.177.83:8080/';
     
     openWebSocket();
+    
+   function createNotification(number, message, link){
+       
+       var date = new Date().toISOString();
+       //.toLocaleDateString('de-DE') 
+       var tag = '<span class="tag is-lite is-pulled-right">'+date+'</span>';
+       var btn = '<button class="delete"></button>';
+       var link ='<a target="_blank" href="'+link+'" style="display:inline-block;width:100%;"><br />'+message+'</a>';
+       
+       return '<div class="notification is-link">'+tag+btn+link+'</div>';
+   }
 
    function openWebSocket(){
     
@@ -13,6 +24,7 @@ $( document ).ready(function() {
     ws.onopen = function () {
     
 	  ws.send('ping');
+	  console.log('ping sent');
     };
     
     ws.onerror = function (error) {
@@ -21,11 +33,16 @@ $( document ).ready(function() {
     
     ws.onmessage = function (e) {
 	console.log('Server: ' + e.data);
-	$('#barcodeTxt').append("<div>"+e.data+"</div>");
+	
+	//$('#wsFrame').prop('src', e.data);
+	
+	var note = createNotification(100, e.data, e.data);
+	$('#barcodeTxt').append(note);
     };
     
-    ws.onclose = function (e) {            
-    $('#barcodeTxt').append('Socket closed');    
+    ws.onclose = function (e) { 
+     
+      $('#barcodeTxt').append('Socket closed');    
      ws = null;
      setTimeout(openWebSocket, 2000);   
     };
