6c49dc2911276ff29d45ea725a4cdf9864aa31de
Max Gaerber <gaerber@axxepta.de>
2019-11-06 17:23:34 +0100

    try more clients

--- websocket/barcode_server.py
+++ websocket/barcode_server.py
@@ -35,7 +35,8 @@ async def do_io():
 start_server = websockets.serve(barcodeFn, "0.0.0.0", 8080)
 
 
-asyncio.get_event_loop().run_until_complete(start_server)
+
 asyncio.get_event_loop().run_until_complete(do_io)
+asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
 
