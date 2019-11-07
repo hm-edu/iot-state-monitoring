600daa1f5793ab417ab8d1d1b5cb0ae20e466fe3
Max <gaerber@axxepta.de>
2019-11-07 13:30:41 +0100

    updated supervisor

--- supervisord/install.sh
+++ supervisord/install.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-sudo apt-get install supervisor
-sudo systemctl enable supervisor
-sudo systemctl start supervisor
+if [ "install" == "${1}" ];then
+    sudo apt-get install supervisor
+    sudo systemctl enable supervisor
+    sudo systemctl start supervisor
+    
+    sudo pip3 install evdev
+    pip3 install evdev
 
-sudo pip3 install evdev
-pip3 install evdev
+    sudo pip3 install pathlib
+    pip3 install pathlib
 
-sudo pip3 install pathlib
-pip3 install pathlib
+    sudo pip3 install websockets
+    pip3 install websockets
 
-sudo pip3 install websockets
-pip3 install websockets
+    sudo pip3 install pyzmq
+    pip3 install pyzmq
 
-sudo pip3 install pyzmq
-pip3 install pyzmq
-
-sudo pip3 install redis
-pip3 install redis
+    sudo pip3 install redis
+    pip3 install redis
+fi
 
 sudo cp barcode.conf /etc/supervisor/conf.d/barcode.conf
-sudo cp barcode_zmq_pub.conf /etc/supervisor/conf.d/barcode_zmq.conf
-sudo cp barcode_zmq_pub.conf /etc/supervisor/conf.d/barcode_zmq_pub.conf
-sudo cp barcode_zmq_sub.conf /etc/supervisor/conf.d/barcode_zmq_sub.conf
+sudo cp barcode-zmq-pub.conf /etc/supervisor/conf.d/barcode-zmq.conf
+sudo cp barcode-zmq-pub.conf /etc/supervisor/conf.d/barcode-zmq_pub.conf
+sudo cp barcode-zmq-sub.conf /etc/supervisor/conf.d/barcode-zmq-sub.conf
+
 sudo supervisorctl update
-sudo cp -rf ../websocket/htdocs /var/www/html/barcode
